b2ad6c81d889e3d5440695567fba5d4a7d79ec2a
Mirfius <88610717+Mirfius@users.noreply.github.com>
2022-11-16 15:36:55 +0300

    Add files via upload

---
+++ kurs_js/kurs1.js
@@ -0,0 +1,34 @@
+"use strict";
+/* */
+let button = document.querySelector(".red__button");  /*находим красную кнопку */
+let button1 = document.querySelector(".bad__button");  /*находим красную кнопку */
+button.addEventListener("click", event => {     /*всякий раз когда кликаем */
+    if(button.classList.contains("active")){
+        button.style.backgroundColor = "gray";
+        button.style.color = "aliceblue";
+        button.classList.remove("active");
+    } else{
+        button.style.backgroundColor = "green";
+        button.style.color = "green";
+        button.classList.add("active"); 
+        delactive1(button1);
+    }
+});
+button1.addEventListener("click", event => {     /*всякий раз когда кликаем */
+if(button1.classList.contains("active")){
+    button1.style.backgroundColor = "gray";
+    button1.style.color = "aliceblue";
+    button1.classList.remove("active");
+    } else{
+        button1.style.backgroundColor = "red";
+        button1.style.color = "red";
+        button1.classList.add("active"); 
+        delactive1(button);
+    }
+});
+
+function delactive1(button) {
+    button.style.backgroundColor = "gray";
+    button.style.color = "aliceblue";
+    button.classList.remove("active");
+  }
